d11719e9f81e9772e2b3be439f787d0635f1c6bf
Alexandre Marcos <alee_ppl@hotmail.com>
2020-08-13 19:07:16 -0300

    init

---
+++ PriceQuotationApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PriceQuotationApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
