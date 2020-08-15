1085df41bf005f610798dc6546aa2591e7ffe981
Alexandre Marcos <alee_ppl@hotmail.com>
2020-08-15 15:43:32 -0300

    First examples; AlphaVantage API usage; SendGrid WEB Api Usage;

---
+++ PriceQuotationApp/AvapiExample.cs
@@ -0,0 +1,51 @@
+﻿using Avapi;
+using Avapi.AvapiTIME_SERIES_DAILY;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PriceQuotationApp
+{
+    class AvapiExample
+    {
+        public static void Example()
+        {
+            // Creating the connection object
+            IAvapiConnection connection = AvapiConnection.Instance;
+
+            // Set up the connection and pass the API_KEY provided by alphavantage.co
+            connection.Connect("API_KEY");
+
+            // Get the TIME_SERIES_DAILY query object
+            Int_TIME_SERIES_DAILY time_series_daily =
+                connection.GetQueryObject_TIME_SERIES_DAILY();
+
+            // Perform the TIME_SERIES_DAILY request and get the result
+            IAvapiResponse_TIME_SERIES_DAILY time_series_dailyResponse =
+            time_series_daily.Query(
+                 "PETR4.SA", //Work SA prices
+                 Const_TIME_SERIES_DAILY.TIME_SERIES_DAILY_outputsize.compact);
+            Console.WriteLine(time_series_dailyResponse);
+            // Printout the results
+            Console.WriteLine("******** RAW DATA TIME_SERIES_DAILY ********");
+            Console.WriteLine(time_series_dailyResponse.RawData);
+
+            Console.WriteLine("******** STRUCTURED DATA TIME_SERIES_DAILY ********");
+            var data = time_series_dailyResponse.Data;
+            if (data.Error)
+            {
+                Console.WriteLine(data.ErrorMessage);
+            }
+            else
+            {
+                Console.WriteLine("Information: " + data.MetaData.Information);
+                Console.WriteLine("Symbol: " + data.MetaData.Symbol);
+                Console.WriteLine("LastRefreshed: " + data.MetaData.LastRefreshed);
+                Console.WriteLine("OutputSize: " + data.MetaData.OutputSize);
+                Console.WriteLine("TimeZone: " + data.MetaData.TimeZone);
+                Console.WriteLine("========================");
+                Console.WriteLine("========================");
+            }
+        }
+    }
+}
--- PriceQuotationApp/Program.cs
+++ PriceQuotationApp/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Threading.Tasks;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace PriceQuotationApp
 {
     class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            AvapiExample.Example();
+
+            await SendEmailAsync();
+        }
+
+        public static async Task SendEmailAsync() {
+            var apiKey = Environment.GetEnvironmentVariable("apikey");
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("test@example.com", "Example User");
+            var subject = "Sending with Twilio SendGrid is Fun";
+            var to = new EmailAddress("test@example.com", "Example User");
+            var plainTextContent = "and easy to do anywhere, even with C#";
+            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg).ConfigureAwait(false);
         }
     }
 }
