9b041e4264a9852a16aa43697c85e6273e0dd948
Alexandre Marcos <alee_ppl@hotmail.com>
2020-08-15 17:52:05 -0300

    Cleaning trash files; Adding SMTP C# Client Example;

--- PriceQuotationApp/Program.cs
+++ PriceQuotationApp/Program.cs
@@ -1,29 +1,56 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using SendGrid;
 using SendGrid.Helpers.Mail;
+using System.Net.Mail;
+using System.Collections.Generic;
+using System.Text;
+using System;
+using System.Net;
 
 namespace PriceQuotationApp
 {
     class Program
     {
-        public static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
             AvapiExample.Example();
 
-            await SendEmailAsync();
+            //await SendEmailAsync();
+
+            SendEmailSMTP();
         }
 
-        public static async Task SendEmailAsync() {
-            var apiKey = Environment.GetEnvironmentVariable("apikey");
+        public static async Task SendEmailAsync()
+        {
+            var apiKey = "apikey";
             var client = new SendGridClient(apiKey);
-            var from = new EmailAddress("test@example.com", "Example User");
-            var subject = "Sending with Twilio SendGrid is Fun";
-            var to = new EmailAddress("test@example.com", "Example User");
-            var plainTextContent = "and easy to do anywhere, even with C#";
-            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var from = new EmailAddress("email", "your name");
+            var subject = "TESTE2";
+            var to = new EmailAddress("email", "receive name");
+            var plainTextContent = "teste";
+            var htmlContent = "<strong>teste</strong>";
             var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
             var response = await client.SendEmailAsync(msg).ConfigureAwait(false);
+        }
+
+        public static void SendEmailSMTP()
+        {
+
+            SmtpClient client = new SmtpClient();
+            client.Port = 587;
+            client.Host = "smtp.sendgrid.net";
+            client.Timeout = 10000;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Credentials = new System.Net.NetworkCredential("apikey", "apikey");
+
+            MailMessage mail = new MailMessage();
+            mail.To.Add(new MailAddress("email@teste.com"));
+            mail.From = new MailAddress("teste@email.com");
+            mail.Subject = "Second Email";
+            mail.Body = "Email via STMP client";
+
+            client.Send(mail);
         }
     }
 }
